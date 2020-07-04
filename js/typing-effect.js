ab4a3dda1ee8782321d9ec90daf573d4f2935b39
Mikki Janower <mikki@janower.net>
2020-07-04 16:19:45 -0400

    filler text --> content

--- js/typing-effect.js
+++ js/typing-effect.js
@@ -10,7 +10,7 @@ new TypeIt("#typed", {
   })
   .type('<a href="https://jewishcurrents.org/author/joshua-leifer/" target="_blank">Jewish Currents</a>. ', {delay: 100})
   .pause(700)
-  .type(' His writing has appeared in <a href="https://www.dissentmagazine.org/" target="_blank">Dissent</a>, <a href="https://www.972mag.com/writer/joshl/" target="_blank">972</a>, <a href="https://nplusonemag.com/authors/leiferjoshua/" target="_blank">n+1</a>, <a href="https://www.theguardian.com/profile/joshua-leifer" target="_blank">The Guardian</a>, <a href="https://www.thenation.com/authors/joshua-leifer/" target="_blank">The Nation</a>, <a href="https://www.jacobinmag.com/author/joshua-leifer" target="_blank">Jacobin</a>,  <a href="https://www.haaretz.com/misc/writers/WRITER-1.6855413" target="_blank">Haaretz</a>, <a href="https://thebaffler.com/authors/joshua-leifer" target="_blank">The Baffler</a>, <a href="https://slate.com/author/joshua-leifer" target="_blank">Slate</a>, and others.')
+  .type(' His work has also appeared in <a href="https://www.theguardian.com/profile/joshua-leifer" target="_blank">The Guardian</a>, <a href="https://www.thenation.com/authors/joshua-leifer/" target="_blank">The Nation</a>, <a href="https://nplusonemag.com/authors/leiferjoshua/" target="_blank">n+1</a>, <a href="https://www.972mag.com/writer/joshl/" target="_blank">+972 Magazine</a>, <a href="https://www.dissentmagazine.org/author/joshua-leifer" target="_blank">Dissent</a>, <a href="https://www.jacobinmag.com/author/joshua-leifer" target="_blank">Jacobin</a>, <a href="https://thebaffler.com/authors/joshua-leifer" target="_blank">The Baffler</a>, <a href="https://www.haaretz.com/misc/writers/WRITER-1.6855413" target="_blank">Haaretz</a>, and elsewhere.')
   .go();
 
   
